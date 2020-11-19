51cd997ea2167f0cc13fde323b7b736dbedbb01f
Douglasx <douglasantana007@gmail.com>
2020-11-19 17:45:15 -0200

    adding setup to seeder

--- api/database/seeds/DatabaseSeeder.php
+++ api/database/seeds/DatabaseSeeder.php
@@ -11,6 +11,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+/*         $this->call([
+            UsersTableSeeder::class,
+            AtividadesTableSeeder::class,
+            PessoasAjudadasTableSeeder::class,
+            RevisaoTableSeeder::class,
+            AtividadePessoaTableSeeder::class
+        ]); */
     }
 }
