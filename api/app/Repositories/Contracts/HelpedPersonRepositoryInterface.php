71b2b4d7be76b93bdf99b8bcac54a0e0033bfd97
douglas <douglasantana007@gmail.com>
2020-11-24 23:00:36 -0300

    [WIP/UPDATE]:wrench: Fixes.

--- api/app/Repositories/Contracts/HelpedPersonRepositoryInterface.php
+++ api/app/Repositories/Contracts/HelpedPersonRepositoryInterface.php
@@ -4,7 +4,7 @@ namespace App\Repositories\Contracts;
 
 interface HelpedPersonRepositoryInterface
 {
+    public function all();
     public function createNewHelpedPerson(array $data);
     public function getHelpedPerson(int $id);
-    public function all();
 }
