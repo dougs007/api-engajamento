a68ea032949e9f514e48df96b9ee2fce2370807d
Rafael José da Costa Glória <rafaeljcg20@gmail.com>
2021-02-17 23:52:03 -0300

    [FIX] Ajustando os nomes das atividades para retratar um pouco melhor o relatório 144421.

--- api/database/seeds/ActivityTableSeeder.php
+++ api/database/seeds/ActivityTableSeeder.php
@@ -13,8 +13,8 @@ class ActivityTableSeeder extends Seeder
     public function run()
     {
         # Activity
-        Activity::create(['tx_nome' => '1-Nome', 'dt_dia' => '2020-09-07 19:00:00']);
-        Activity::create(['tx_nome' => '4-Pessoas', 'dt_dia' => '2020-09-08 20:30:00']);
+        Activity::create(['tx_nome' => '1-Revisionista', 'dt_dia' => '2020-09-07 19:00:00']); # 
+        Activity::create(['tx_nome' => '4-Nomes', 'dt_dia' => '2020-09-08 20:30:00']);
         Activity::create(['tx_nome' => '4-Mensagens', 'dt_dia' => '2020-09-09 19:30:00']);
         Activity::create(['tx_nome' => '4-Ligações', 'dt_dia' => '2020-09-10 17:00:00']);
         Activity::create(['tx_nome' => '2-Visitas', 'dt_dia' => '2020-09-11 18:00:00']);
